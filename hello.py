62fe382f235ae98ba44c089116c367d34b5ea80d
Albert Nguyen <alberthuyt@gmail.com>
2015-05-07 22:53:54 -0700

    intial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# hello program 
+
+def PrintHello():
+    """
+    print hello
+    """
+
+    print("hello")
+
+# Start program
+def main():
+    PrintHello()
+
+if __name__ == "__main__":
+    main()
+
