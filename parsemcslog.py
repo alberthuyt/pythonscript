095834895ba1f62977ca60e0bfd4cbdc96147b79
Albert Nguyen <alberthuyt@gmail.com>
2015-05-08 00:01:04 -0700

    add argparse

--- parsemcslog.py
+++ parsemcslog.py
@@ -4,18 +4,25 @@
 
 import os, sys
 import re
+import argparse
 
 def CheckArg():
-    if len(sys.argv) != 2:
-        return True
+    # if len(sys.argv) != 2:
+    #     return True
+
+    parser = argparse.ArgumentParser(description='Process args')
+    parser.add_argument('-f', '--file', required=True, action='store', help='parse log file')   
+    args = parser.parse_args() 
+    return args
 
 def ReadMCSLog():
     """
     Open and read MCS log file
     """
+    CheckArg()
 
-    if CheckArg():
-        sys.exit("you need to intput fullpath to log file as argument")
+    # if CheckArg():
+    #     sys.exit("you need to intput fullpath to log file as argument")
 
     w = "^ERROR|^FATAL|^WARNING"
     with open(sys.argv[1]) as f:
@@ -27,6 +34,7 @@ def main():
     """
     start program
     """
+    args = CheckArg()
     ReadMCSLog()
 
 if __name__ == "__main__":
