91ba6969893184eb60f50b1d2589ece31fd38814
desktopwork <jtsupro@gmail.com>
2015-05-12 00:34:44 -0700

    add test delete proxy

--- dmrestapi/test_dprestapi.py
+++ dmrestapi/test_dprestapi.py
@@ -28,7 +28,7 @@ class DMsession:
         # logging.info(self.headers)
         if self.response.status_code == 200:
             self.headers["X-CustomTicket"] = self.response.content[1:-1]
-            logging.info(self.response.text)
+            # logging.info(self.response.text)
         return self.response
 
 def test_logout():
@@ -55,17 +55,19 @@ def test_list_proxy():
     list_proxy_service = "deploymanager/proxy"
     url = urljoin(dmsession.url, list_proxy_service)
 
-    logging.info(dmsession.headers)
-    logging.info(url)
+    # logging.info(dmsession.headers)
+    # logging.info(url)
 
     response = requests.get(url, \
         headers=dmsession.headers, verify=False)
 
     
-    logging.info(dmsession.headers)
-
+    # logging.info(dmsession.headers)
+    # logging.info(response.content)
 
     assert response.status_code == 200
+    return response.content
+
 
 def test_create_recommend():
     dmsession = DMsession()
@@ -83,11 +85,29 @@ def test_create_recommend():
     logging.info(recommend_config)
     assert response.status_code == 202
 
+def test_delete_proxy():
+    dmsession = DMsession()
+    response = dmsession.login()
+    assert response.status_code == 200 
+    # ujson.loads load string json and change into python
+    # ujson.dumps load python and serialize to json
+    list_proxy = ujson.loads(test_list_proxy())
+    for proxy in list_proxy:
+        if 'proxy4' in proxy['name']:
+            delete_service = "{}/{}".format("deploymanager/proxy", proxy['instanceUUID'])
+
+            logging.info(delete_service)
+            url = urljoin(dmsession.url, delete_service)
+            response = requests.delete(url, headers=dmsession.headers, verify=False)
+            assert response.status_code == 202
+
 def main():
 
-    test_logout()
-    test_list_proxy()
-    test_create_recommend()
+    # test_logout()
+    # test_list_proxy()
+    # test_create_recommend()
+
+    test_delete_proxy()
 
 
 if __name__ == "__main__":
